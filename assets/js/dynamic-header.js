98330173a3e0e2d94568e77448cd88ded61f2faf
DAHCROWN <dsilakass@gmail.com>
2022-05-01 14:41:20 +0100

    hub pages

---
+++ assets/js/dynamic-header.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $(".header__bg__layer").addClass("active");
+      } else {
+          //remove the background property so it comes transparent again (defined in your css)
+         $(".header__bg__layer").removeClass("active");
+      }
+  });
+});

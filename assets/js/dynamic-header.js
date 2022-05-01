fdfbab3539da819f4beda788ba28668778cf230f
DAHCROWN <dsilakass@gmail.com>
2022-05-01 20:10:38 +0100

    day 4 final

--- assets/js/dynamic-header.js
+++ assets/js/dynamic-header.js
@@ -1,7 +1,7 @@
 $(function() {
   $(window).on("scroll", function() {
       if($(window).scrollTop() > 50) {
-          $(".header__bg__layer").addClass("active");
+          $(".header__body").addClass("active");
       } else {
           //remove the background property so it comes transparent again (defined in your css)
          $(".header__bg__layer").removeClass("active");

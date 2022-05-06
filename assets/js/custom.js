c51ec26a6aa6ffe5d9897fabb7a8c5ec28f5e82a
Esla dan <alsenad390@gmail.com>
2022-05-06 10:56:08 +0100

    new backgrounds

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,21 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var box = $('.hero-bg').height();
-    var header = $('header').height();
+// $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//     var box = $('.hero-bg').height();
+//     var header = $('header').height();
     
-if (box) {
-    if (scroll >= box - header ) {        
-      $(".header__body").addClass("background-header");
-    } else {
-      $(".header__body").removeClass("background-header");
-    }
-}else{
-  if(scroll >= header){
-    $(".header__body").addClass("header-position");
-  }
-  else{
-    $(".header__body").removeClass("header-position");
-  }
-}
+// if (box) {
+//     if (scroll >= box - header ) {        
+//       $(".header__body").addClass("background-header");
+//     } else {
+//       $(".header__body").removeClass("background-header");
+//     }
+// }else{
+//   if(scroll >= header){
+//     $(".header__body").addClass("header-position");
+//   }
+//   else{
+//     $(".header__body").removeClass("header-position");
+//   }
+// }
 
-});
+// });

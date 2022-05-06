5a9df60609c0c285e860fb7c4440f6541ed4a829
Esla dan <alsenad390@gmail.com>
2022-05-06 14:15:13 +0100

    owl fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,3 +19,58 @@
 // }
 
 // });
+
+
+var owlClass = document.querySelector('.owl-carousel')
+
+if (owlClass) {
+
+
+  jQuery(document).ready(function ($) {
+    var owl = $('.owl-carousel');
+    owl.on('initialize.owl.carousel initialized.owl.carousel ' +
+      'initialize.owl.carousel initialize.owl.carousel ' +
+      'resize.owl.carousel resized.owl.carousel ' +
+      'refresh.owl.carousel refreshed.owl.carousel ' +
+      'update.owl.carousel updated.owl.carousel ' +
+      'drag.owl.carousel dragged.owl.carousel ' +
+      'translate.owl.carousel translated.owl.carousel ' +
+      'to.owl.carousel changed.owl.carousel',
+      function (e) {
+        $('.' + e.type)
+          .removeClass('secondary')
+          .addClass('success');
+        window.setTimeout(function () {
+          $('.' + e.type)
+            .removeClass('success')
+            .addClass('secondary');
+        }, 500);
+      });
+    owl.owlCarousel({
+      loop: false,
+      // center: true,
+      nav: true,
+      lazyLoad: true,
+      margin: 150,
+      video: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true,
+        },
+        600: {
+          items: 3,
+          nav: true,
+        },
+        960: {
+          items: 4,
+        },
+        1200: {
+          items: 5
+        }
+      }
+    });
+
+  });
+
+}
